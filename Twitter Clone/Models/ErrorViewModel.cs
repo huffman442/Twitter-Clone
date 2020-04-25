3c596dce08ca63339cb5cae6c82d1a5935e02bc5
Michael Huffman <huffman442@yahoo.com>
2020-04-25 09:47:26 -0400

    Add project files.

---
+++ Twitter Clone/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Twitter_Clone.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
